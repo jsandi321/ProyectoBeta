1ee3163f4758f2f04a8f7a51a54edac5e8ab1b91
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2019-10-16 20:51:11 -0600

    Actualizacion de clase Battleship (main)
    Clase Main que enlaza las clases, pruebas

--- battleship/Battleship.java
+++ battleship/Battleship.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package battleship;
+import java.util.Scanner;
 
 /**
  *
@@ -16,10 +17,22 @@ public class Battleship {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Player player1 = new Player("Jeison");
-        Player player2 = new Player("Jack");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola Bienvenidos a Battleship");
+        System.out.println("Jugador 1 por favor introduce tu nombre: ");
+        String name1 = sc.next();
+        System.out.println("Jugador 2 por favor introduce tu nombre: ");
+        String name2 = sc.next();
+        Player player1 = new Player(name1);
+        Player player2 = new Player(name2);
         System.out.println("Hola jugador 1 "+player1.getName());
         System.out.println("Hola jugador 2 "+player2.getName());
+        player1.generateShips();
+        player2.generateShips();
+        player1.generateBoard(player1.getPrincipalBoard());
+        player1.generateBoard(player1.getShootsBoard());
+        player1.showShipsBoard();
+        player1.showShootBoard();
     }
     
 }
