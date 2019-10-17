d59fc311f24004f93aaa2334204a85f95713d341
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2019-10-16 20:39:42 -0600

    Actualización de la clase Player
    Se agregaron métodos para generar y mostrar en pantalla los tableros.
    Nuevo atributo arrayList de la clase para almacenar los objetos tipo Ship.
    Metodo para generar todos los barcos necesarios para el juego

--- battleship/Player.java
+++ battleship/Player.java
@@ -4,7 +4,7 @@
  * los barcos y otro de disparos para cada jugador.
  */
 package battleship;
-
+import java.util.ArrayList;
 /**
  *
  * @author jsandi321
@@ -19,7 +19,7 @@ public class Player {
     //Definir si seran atribustos u objetos
     private String principalBoard[][]= new String[10][10];
     private String shootsBoard[][]= new String[10][10];
-    
+    private ArrayList<Ship> ships = new ArrayList();
     //Constructores de la clase
     public Player(String name){
         this.name = name;
@@ -47,11 +47,11 @@ public class Player {
  * Descripcion: Metodo que rellena el tablero de disparos son Strings que se 
  * reemplazaran por lo disparos del jugador.
  */
-    public void shootBoardField(){
-        for(int i = 0; i < shootsBoard.length; i++){
-            for(int j = 0; j < shootsBoard.length; j++){
+    public void generateBoard(String[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
                 String field = "~";
-                shootsBoard[i][j] = field;
+                board[i][j] = field;
             }
         }
     }
@@ -69,5 +69,43 @@ public class Player {
         int x = pos[0];
         int y = pos[1];
         shootsBoard[x][y] = shoot;
+    }
+    public void showShootBoard(){
+        System.out.println("Este es tu tablero de disparos");
+        for (int x=0; x < shootsBoard.length; x++) {
+            System.out.print("|");
+            for (int y=0; y < shootsBoard[x].length; y++) {
+                System.out.print (shootsBoard[x][y]);
+                if (y!=shootsBoard[x].length-1) System.out.print("  ");
+            }
+            System.out.println("|");
+        }
+    }
+    public void showShipsBoard(){
+        System.out.println("Este es tu tablero de barcos");
+        for (int x=0; x < principalBoard.length; x++) {
+            System.out.print("|");
+            for (int y=0; y < principalBoard[x].length; y++) {
+                System.out.print (principalBoard[x][y]);
+                if (y!=principalBoard[x].length-1) System.out.print("  ");
+            }
+            System.out.println("|");
+        }
+    }
+    public void generateShips(){
+        Ship portaAviones = new Ship("portaAviones" , 4);
+        ships.add(portaAviones);
+        for (int i = 0; i < 3; i++){
+            Ship submarine = new Ship("submarino"+i, 3);
+            ships.add(submarine);
+        }
+        for (int i = 0; i < 3; i++){
+            Ship destroyer = new Ship("destructor"+i, 2);
+            ships.add(destroyer);
+        }
+        for(int i = 0; i < 2; i++){
+            Ship frigate = new Ship ("fragata"+i, 1);
+            ships.add(frigate);
+        }
     }
 }
